a1605275e61bf7ef91ede74df90fa30cc57f7018
paroaro <patrik.pavic2@gmail.com>
2020-11-26 10:41:27 +0100

    Dodao Pavić rješenje

---
+++ F/pavic.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+typedef long double ld;
+
+const int N = 1e5 + 500;
+
+ld r;
+int n;
+vector < pair < ld, int > > v;
+
+int main(){
+	scanf("%d%Lf", &n, &r);
+	for(int i = 0;i < n;i++){
+		ld x, y; scanf("%Lf%Lf", &x, &y);
+		if(y <= r){
+			ld pom = sqrt(r * r - y * y);
+			v.push_back({x + pom, 1});
+			v.push_back({x - pom, 0});
+		}
+	}
+	sort(v.begin(), v.end());
+	int sol = 0, cur = 0;
+	for(pair < ld, int > tmp : v){
+		if(tmp.second)
+			cur--;
+		else
+			cur++;
+		sol = max(sol, cur);
+	}
+	printf("%d\n", sol);
+	return 0;
+}
